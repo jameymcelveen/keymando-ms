a4de8292e328ea16670d8c49f326c117cbcae66e
Jamey McElveen <Jamey.McElveen@ACSTechnologies.com>
2012-09-21 11:26:43 -0400

    Add config file

---
+++ keymando-config.rb
@@ -0,0 +1,19 @@
+# Start Keymando at login
+# -----------------------------------------------------------
+start_at_login
+
+# Windows Mappings
+# -----------------------------------------------------------
+
+# Clipboard
+map "<Ctrl-c>", "<Cmd-c>"
+map "<Ctrl-v>", "<Cmd-v>"
+map "<Ctrl-x>", "<Cmd-x>"
+
+# Redo & Undo
+map "<Ctrl-r>", "<Cmd-r>"
+map "<Ctrl-z>", "<Cmd-z>"
+
+# Back / Forward Arrow Nav
+map "<Home>", "<Cmd-Left>"
+map "<End>", "<Cmd-Right>"
